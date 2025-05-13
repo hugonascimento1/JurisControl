d57fb85013393d6be77b3b42f7cfb307f157bf55
Hugo Nascimento <hugovinitek@gmail.com>
2025-05-12 21:27:21 -0300

    Update login-form

--- src/components/ui/login-form.tsx
+++ src/components/ui/login-form.tsx
@@ -5,66 +5,133 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
+import { Tabs, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import { TabsList } from "./tabs";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function LoginForm() {
 
     const router = useRouter();
 
-    const handleLogin = () =>  {
+    const handleLogin = () => {
         router.push('/inicio');
     }
 
     return (
-        <form action="" className="space-y-3">
-            <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-                <h1 className={`mb-3 text-2xl text-center font-normal text-[#030430]`}>
-                    <strong>Entrar</strong>
-                </h1>
-                <div className="w-full">
-                    <div>
-                        <Label htmlFor="email" className="mb-2 mt-5 block text-lg font-semibold text-gray-900">
-                            Email
-                        </Label>
-                        <div className="relative">
-                            <Input 
-                                className="peer block w-full rounded-md border-2 border-gray-500 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                type="text"
-                                id="cpf"
-                                name="cpf"
-                                placeholder="Digite seu CPF"
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className="mt-4">
-                        <Label htmlFor="password" className="mb-2 mt-5 block text-lg font-semibold text-gray-900">
-                            Senha
-                        </Label>
-                        <div className="relative">
-                            <Input 
-                                className="peer block w-full rounded-md border-2 border-gray-500 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                type="text" 
-                                id="password"
-                                name="password"
-                                placeholder="Digite sua Senha"
-                                required
-                                minLength={6}
-                            />
-                        </div>        
-                    </div>
-                </div>
-                <Button 
-                    className="mt-5 text-base w-full h-11 bg-[#030430]"
-                    type="submit"
-                    onClick={handleLogin}
-                >
-                    Entrar 
-                    <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-                </Button>
-                <div className="flex h-8 items-end space-x-1">
-                    {/* Aqui ficarão as mensagem de erro  */}
-                </div>
-            </div>
-        </form>
+        <div>
+            <Tabs defaultValue="loginAdvogado" className="w-[400px]">
+                <TabsList className="grid w-full grid-cols-2 bg-gray-300 h-12 rounded-lg">
+                    <TabsTrigger value="loginAdvogado" className=" data-[state=active]:bg-[#030430] data-[state=active]:text-white text-gray-700 hover:text-gray-900">Advogado</TabsTrigger>
+                    <TabsTrigger value="loginAdministrador" className=" data-[state=active]:bg-[#030430] data-[state=active]:text-white text-gray-700 hover:text-gray-900">Administrador</TabsTrigger>
+                </TabsList>
+                <TabsContent value="loginAdvogado">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle className="text-center text-2xl text-[#030430] font-semibold">Login Advogado</CardTitle>
+                            {/* <CardDescription>Faça login como administrador</CardDescription> */}
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <div>
+                                <Label htmlFor="email" className="mb-2 mt-5 block text-lg font-semibold text-gray-900">
+                                    Email
+                                </Label>
+                                <Input
+                                    className="peer block w-full rounded-md border-2 border-gray-500 py-[9px] text-sm outline-2 placeholder:text-gray-500"
+                                    type="text"
+                                    id="email"
+                                    name="email"
+                                    placeholder="Insira seu email"
+                                    required
+                                />
+                            </div>
+                            <div className="">
+                                <Label htmlFor="password" className="mb-2 mt-5 block text-lg font-semibold text-gray-900">
+                                    Senha
+                                </Label>
+
+                                <Input
+                                    className="peer block w-full rounded-md border-2 border-gray-500 py-[9px] text-sm outline-2 placeholder:text-gray-500"
+                                    type="text"
+                                    id="password"
+                                    name="password"
+                                    placeholder="Insira sua senha"
+                                    required
+                                    minLength={6}
+                                />
+
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button
+                                className=" text-base w-full h-11 bg-[#030430]"
+                                type="submit"
+                                onClick={handleLogin}
+                            >
+                                Entrar
+                                <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+                            </Button>
+                            <div className="flex h-8 items-end space-x-1">
+                                {/* Aqui ficarão as mensagem de erro  */}
+                            </div>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+
+                <TabsContent value="loginAdministrador">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle className="text-center text-2xl text-[#030430] font-semibold">Login Admin</CardTitle>
+                            {/* <CardDescription>Faça login como administrador</CardDescription> */}
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <div>
+                                <Label htmlFor="email" className="mb-2 mt-5 block text-lg font-semibold text-gray-900">
+                                    Email
+                                </Label>
+                                <Input
+                                    className="peer block w-full rounded-md border-2 border-gray-500 py-[9px] text-sm outline-2 placeholder:text-gray-500"
+                                    type="text"
+                                    id="email"
+                                    name="email"
+                                    placeholder="Insira seu email"
+                                    required
+                                />
+                            </div>
+                            <div className="">
+                                <Label htmlFor="password" className="mb-2 mt-5 block text-lg font-semibold text-gray-900">
+                                    Senha
+                                </Label>
+
+                                <Input
+                                    className="peer block w-full rounded-md border-2 border-gray-500 py-[9px] text-sm outline-2 placeholder:text-gray-500"
+                                    type="text"
+                                    id="password"
+                                    name="password"
+                                    placeholder="Insira sua senha"
+                                    required
+                                    minLength={6}
+                                />
+
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button
+                                className=" text-base w-full h-11 bg-[#030430]"
+                                type="submit"
+                                onClick={handleLogin}
+                            >
+                                Entrar
+                                <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+                            </Button>
+                            <div className="flex h-8 items-end space-x-1">
+                                {/* Aqui ficarão as mensagem de erro  */}
+                            </div>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+            </Tabs>
+
+        </div>
+
     )
 }
